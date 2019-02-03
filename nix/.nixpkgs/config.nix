0bdf40ca6f95d3060ea43c9d05ec53ccdedc18f5
Erik Ivarsson <eriiva@outlook.com>
2019-02-03 19:36:36 +0100

    Add desktop environment

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -2,11 +2,17 @@
   allowUnfree = true;
 
   packageOverrides = pkgs_: with pkgs_; {
-    all = buildEnv {
+    base = buildEnv {
       name = "base";
       paths = [
         (neovim.override { vimAlias=true; }) fzf silver-searcher bear vimPlugins.fzfWrapper vimPlugins.fzf-vim vimPlugins.YouCompleteMe
         file
+      ];
+    };
+    desktop = buildEnv {
+      name = "desktop";
+      paths = [
+        firefox wireshark
       ];
     };
   };
