619bc656b06036fb826985ce2fe80408ac9f6cce
Erik Ivarsson <eriiva@outlook.com>
2019-02-03 19:34:42 +0100

    Correct issues in setup script

--- setup.sh
+++ setup.sh
@@ -5,29 +5,29 @@ help() {
 }
 
 setup() {
-  local pkg=$1
-  local target=$2
+  pkg=$1
+  target=$2
   cd "$pkg" || return 1
 
-  local dirs="$(find . -mindepth 1 -type d | sed "s|./||")"
+  dirs="$(find . -mindepth 1 -type d | sed "s|./||")"
   for d in $dirs; do
     mkdir -p "$target/$d"
     echo "Created directory $target/$d"
   done
 
-  local files="$(find . -type f | sed "s|./||")"
+  files="$(find . -type f | sed "s|./||")"
   for f in $files; do
-    local targetf="$target/$f"
-    local sourcef="$(pwd)/$f"
+    targetf="$target/$f"
+    sourcef="$(pwd)/$f"
     ln -b -s "$sourcef" "$targetf" || return 1
     echo "Linked file $f"
   done
-}  
+}
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   help
   exit 1
 fi
 
-cd $(dirname "$0") || return 1
+cd "$(dirname "$0")" || return 1
 setup "$1" "$HOME"
